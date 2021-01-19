27a5a4344e28cf85faffbf05db08b91e84d3a3e1
GwanWoo-Jang <21500615@handong.edu>
2021-01-20 01:35:00 +0900

    0120추가

--- a.py
+++ a.py
@@ -1,5 +1,29 @@
-def function():
-        print("a")
+def fight():
+    turn = 0
+    while True:
+        break
 
 
-function()
+class Unit:
+    def __init__(self, name, attack, defence, health, speed, chance_ch, damage_ch, effect, resistance_effect, chance_dual ):
+        self.name = name
+        self.
+        self.attack = attack
+        self.defence = defence
+        self.health = health
+        self.speed = speed
+        self.chance_ch = chance_ch * 0.01
+        self.damage_ch = damage_ch * 0.01
+        self.effect = resistance_effect * 0.01
+        self.chance_dual = chance_dual *0.01
+        print(name + " 생성")
+        def skill1():
+                
+
+my_unit1 = Unit("생성된 유닛1", )
+enermy_unit1 = Unit("생성된 유닛2", 1000, , 
+
+
+
+
+        
